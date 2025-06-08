2d519984a5cb1d8f1c4311905929ecea9d274abd
diviravindranath <diviravindranath@gmail.com>
2025-06-08 09:09:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,8 +19,8 @@ export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-org: "jsmpro",
-project: "jsm_converso",
+org: "start-your-hub-vy",
+project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
